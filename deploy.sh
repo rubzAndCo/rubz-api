32ad3f5662d973f48fa1f87280530dda1efe7437
Ruben DESERT <ruben.desert@gmail.com>
2020-08-14 14:37:04 +0200

    add deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export PATH=$PATH:/usr/local/bin
+export NODE_PATH=/usr/local/share/node
+export USER=root
+
+source $HOME/.nvm/nvm.sh
+
+cd /var/www/api || exit
+
+nvm use --lts
+
+/usr/bin/env npm install
+/usr/bin/env npm run build
+/usr/bin/env npm run start:prod
